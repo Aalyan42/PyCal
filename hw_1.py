64ae3e874c1dda8c9c6aa1d1fb43685c80f23ade
Aalyan Aamir <aalyan@unknown-air.lan>
2024-02-03 13:42:32 -0800

    Updating and adding homework2

---
+++ hw_1.py
@@ -0,0 +1,7 @@
+print((.000000001*.987654321)*(.0909090909**.000001))
+
+#reason: visualing probability of the plan making it out of the group chat
+#filename: cause its the first hw gotta make it memorable yk
+#how to run: "python hw_1.py"
+
+
