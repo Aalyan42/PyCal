acda23fa1b2144c9b458ef40ad273ba6bd5cfddb
Aalyan42 <62355859+Aalyan42@users.noreply.github.com>
2024-02-17 11:30:08 -0800

    Delete hw_1.py

--- hw_1.py
+++
@@ -1,7 +0,0 @@
-print((.000000001*.987654321)*(.0909090909**.000001))
-
-#reason: visualing probability of the plan making it out of the group chat
-#filename: cause its the first hw gotta make it memorable yk
-#how to run: "python hw_1.py"
-
-
